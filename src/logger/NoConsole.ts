4b897974b0b7ae261cc129db45e8592e2ccd592c
sgohlke <gohlke.stefan@yahoo.de>
2025-03-14 11:43:09 +0100

    Fix linting issue

--- src/logger/NoConsole.ts
+++ src/logger/NoConsole.ts
@@ -3,6 +3,7 @@ import { Console } from 'node:console'
 /**
  * A console that does nothing
  */
+// eslint-disable-next-line eslint-plugin-import/exports-last
 export const NoConsole = new Console(process.stdout)
 NoConsole.log = (): void => {
     // do nothing
